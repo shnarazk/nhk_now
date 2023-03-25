78153eaf420790a4ad83a748523965d3190a1cc8
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-03-25 14:41:05 +0900

    got a JSON

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use {clap::Parser, dioxus::prelude::*, hyper::Client, hyper_tls::HttpsConnector};
+use {
+    clap::Parser, dioxus::prelude::*, hyper::Client, hyper_tls::HttpsConnector, serde_json::Value,
+};
 
 #[derive(Clone, Debug, Default, Parser)]
 #[clap(author, version, about)]
@@ -25,19 +27,20 @@ async fn main() {
     let config = Config::parse();
     println!("Hello, world!");
     dbg!(&config);
-    let _ = load_json(&config);
+    let Ok(json) = load_json(&config).await else { panic!("failed to get a JSON");};
+    dbg!(&json);
 }
 
-async fn load_json(config: &Config) -> hyper::Result<()> {
+async fn load_json(config: &Config) -> hyper::Result<Value> {
     let area = config.area.as_deref().unwrap_or("400");
     let service = config.service.as_deref().unwrap_or("g1");
     let key = &config.key;
-    let base = format!(
-        "https://https://api.nhk.or.jp/v2/pg/now/{area}/{service}.json?key={api_key}" // "https://https://api.nhk.or.jp/v2/pg/list/{area}/{service}/{date}.json?key={key}"
-    );
+    // "https://api.nhk.or.jp/v2/pg/list/{area}/{service}/{date}.json?key={key}"
+    let base = format!("https://api.nhk.or.jp/v2/pg/now/{area}/{service}.json?key={key}");
     let client = Client::builder().build::<_, hyper::Body>(HttpsConnector::new());
     let res = client.get(base.parse().expect("wrong url")).await?;
     let buf = hyper::body::to_bytes(res).await?;
-    let _str = String::from_utf8_lossy(buf.as_ref());
-    Ok(())
+    let str = String::from_utf8_lossy(buf.as_ref());
+    let json: Value = serde_json::from_str(str.to_string().as_str()).expect("invalid json");
+    Ok(json)
 }
