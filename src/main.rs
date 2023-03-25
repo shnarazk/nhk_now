8d28ac628d71f4b529b19f3be8b4c10ef5d5b4f4
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-03-25 14:00:25 +0900

    add src

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use {clap::Parser, dioxus::prelude::*, hyper::Client, hyper_tls::HttpsConnector};
+
+#[derive(Clone, Debug, Default, Parser)]
+#[clap(author, version, about)]
+struct Config {
+    /// area code
+    #[clap(short = 'a')]
+    area: Option<String>,
+    /// service (channel)
+    #[clap(short = 's')]
+    service: Option<String>,
+    /// date
+    #[clap(short = 'd')]
+    date: Option<String>,
+    /// API key
+    #[clap(short = 'k', long)]
+    key: String,
+    /// Just download the csv w/o GUI
+    #[clap(long = "headless")]
+    headless: bool,
+}
+
+#[tokio::main]
+async fn main() {
+    let config = Config::parse();
+    println!("Hello, world!");
+    dbg!(&config);
+    let _ = load_json(&config);
+}
+
+async fn load_json(config: &Config) -> hyper::Result<()> {
+    let area = config.area.as_deref().unwrap_or("400");
+    let service = config.service.as_deref().unwrap_or("g1");
+    let key = &config.key;
+    let base = format!(
+        "https://https://api.nhk.or.jp/v2/pg/now/{area}/{service}.json?key={api_key}" // "https://https://api.nhk.or.jp/v2/pg/list/{area}/{service}/{date}.json?key={key}"
+    );
+    let client = Client::builder().build::<_, hyper::Body>(HttpsConnector::new());
+    let res = client.get(base.parse().expect("wrong url")).await?;
+    let buf = hyper::body::to_bytes(res).await?;
+    let _str = String::from_utf8_lossy(buf.as_ref());
+    Ok(())
+}
