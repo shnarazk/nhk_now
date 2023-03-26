ae4477d1701e09a216f9c6fe3d15e2abee29f5c0
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-03-26 15:42:43 +0900

    use daisyui

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ async fn main() {
     dioxus_desktop::launch_cfg(
         app,
         Config::new()
-            .with_custom_head("<script src=\"https://cdn.tailwindcss.com\"></script>".to_string()),
+            .with_custom_head("<link href=\"https://cdn.jsdelivr.net/npm/daisyui@2.51.5/dist/full.css\" rel=\"stylesheet\" type=\"text/css\" />\n<script src=\"https://cdn.tailwindcss.com\"></script>".to_string()),
     );
 }
 
@@ -46,91 +46,108 @@ fn app(cx: Scope) -> Element {
             // dbg!(&json["nowonair_list"]["g1"]["present"]);
             // dbg!(&json["nowonair_list"]["g1"]);
             cx.render(rsx!{
-                h1 {
-                    class: "inline-block px-10 bg-slate-200 m-4 text-lg text-red-600 drop-shadow-xl border-solid border-2 border-indigo-600 rounded",
-                    "NHK綜合プログラム"
-                }
-                table {
-                    class: "bg-red-600 border-red-600 border-y-2 px-10 m-10 text-white drop-shadow-xl border-solid border-0 border-indigo-600 rounded",
-                    tr {
-                        class: "px-2",
-                        th {
-                            "開始時間"
-                        }
-                        th {
-                            class: "pl-10",
-                            "program"
-                        }
+                div {
+                    class: "tabs mt-2 ml-2",
+                    // class: "btn-group ml-4 mt-4",
+                    button {
+                        class: "tab tab-lifted tab-active text-lg",
+                        // class: "btn bg-secondary focus:bg-secondary no-animation",
+                        "NHK総合プログラム"
                     }
-                    tr {
-                        class: "bg-slate-100 text-gray-600",
-                        td {
-                            class: "px-2",
-                            DateTime::parse_from_rfc3339(json["nowonair_list"]["g1"]["following"]["start_time"].as_str().unwrap()).unwrap().format("%H:%M").to_string(),
-                        }
-                        td {
-                            class: "pl-6 pr-2",
-                            json["nowonair_list"]["g1"]["following"]["title"].as_str(),
-                        }
-                    }
-                    tr {
-                        class: "bg-slate-100 text-gray-600",
-                        td {
-                            colspan: 2,
-                            class: "pl-20 pr-2",
-                            json["nowonair_list"]["g1"]["following"]["subtitle"].as_str(),
-                        }
-                    }
-                    tr {
-                        class: "bg-slate-200 text-black",
-                        td {
-                            class: "px-2",
-                            DateTime::parse_from_rfc3339(json["nowonair_list"]["g1"]["present"]["start_time"].as_str().unwrap()).unwrap().format("%H:%M").to_string(),
-                        }
-                        td {
-                            class: "pl-6 pr-2",
-                            json["nowonair_list"]["g1"]["present"]["title"].as_str(),
-                        }
-                    }
-                    tr {
-                        class: "bg-slate-200 text-black",
-                        td {
-                            colspan: 2,
-                            class: "pl-20 pr-2",
-                            json["nowonair_list"]["g1"]["present"]["subtitle"].as_str(),
-                        }
-                    }
-                    tr {
-                        class: "bg-slate-400 text-gray-800",
-                        td {
-                            class: "px-2",
-                            DateTime::parse_from_rfc3339(json["nowonair_list"]["g1"]["previous"]["start_time"].as_str().unwrap()).unwrap().format("%H:%M").to_string(),
-                        }
-                        td {
-                            class: "pl-6 pr-2",
-                            json["nowonair_list"]["g1"]["previous"]["title"].as_str(),
-                        }
-                    }
-                    tr {
-                        class: "bg-slate-400 text-gray-800",
-                        td {
-                            colspan: 2,
-                            class: "pl-20 pr-2",
-                            json["nowonair_list"]["g1"]["previous"]["subtitle"].as_str(),
-                        }
+                    button {
+                        class: "tab tab-lifted text-lg",
+                        // class: "btn bg-secondary no-animation",
+                        "Eテレ"
                     }
                 }
                 div {
-                    class: "text-center ",
-                    button {
-                        class: "p-2 my-4 bg-indigo-500 text-white fond-bold rounded-lg border shadow-lg",
-                        onclick: move |_| { count += 1 },
-                        "reload"
+                    class: "grid bg-base-300 p-0 mx-2 drop-shadow-xl",
+                    table {
+                        class:"table table-compact p-0 mt-0 w-full text-white bg-red-600 border-red-600 border-y-2 border-solid border-0 border-indigo-600",
+                        tr {
+                            class:"",
+                            th {
+                                "開始時間"
+                            }
+                            th {
+                                class:"text-right",
+                                button {
+                                    class: "btn btn-ghost btn-outline btn-sm",
+                                    onclick: move |_| { count += 1 },
+                                    "プログラム更新"
+                                }
+                            }
+                        }
+                        tr {
+                            class:"bg-slate-100 text-gray-600",
+                            td {
+                                class:"",
+                                DateTime::parse_from_rfc3339(json["nowonair_list"]["g1"]["following"]["start_time"].as_str().unwrap()).unwrap().format("%H:%M").to_string(),
+                            }
+                            td {
+                                class:"",
+                                json["nowonair_list"]["g1"]["following"]["title"].as_str(),
+                            }
+                        }
+                        tr {
+                            class:"bg-slate-100 text-gray-600",
+                            td {
+                                colspan: 2,
+                                class:"whitespace-normal pl-8 w-4/5 text-sm",
+                                json["nowonair_list"]["g1"]["following"]["subtitle"].as_str(),
+                            }
+                        }
+                        tr {
+                            class:"bg-slate-200 text-black",
+                            td {
+                                class:"",
+                                DateTime::parse_from_rfc3339(json["nowonair_list"]["g1"]["present"]["start_time"].as_str().unwrap()).unwrap().format("%H:%M").to_string(),
+                            }
+                            td {
+                                class:"",
+                                json["nowonair_list"]["g1"]["present"]["title"].as_str(),
+                            }
+                        }
+                        tr {
+                            class:"bg-slate-200 text-black",
+                            td {
+                                colspan: 2,
+                                class:"whitespace-normal pl-8 w-4/5 text-sm",
+                                json["nowonair_list"]["g1"]["present"]["subtitle"].as_str(),
+                            }
+                        }
+                        tr {
+                            class:"bg-slate-400 text-gray-800",
+                            td {
+                                class:"",
+                                DateTime::parse_from_rfc3339(json["nowonair_list"]["g1"]["previous"]["start_time"].as_str().unwrap()).unwrap().format("%H:%M").to_string(),
+                            }
+                            td {
+                                class:"",
+                                json["nowonair_list"]["g1"]["previous"]["title"].as_str(),
+                            }
+                        }
+                        tr {
+                            class:"bg-slate-400 text-gray-800",
+                            td {
+                                colspan: 2,
+                                class:"whitespace-normal pl-8 w-4/5 text-sm",
+                                json["nowonair_list"]["g1"]["previous"]["subtitle"].as_str(),
+                            }
+                        }
                     }
                 }
             })
         }
-        _ => cx.render(rsx!(h1 { "..."})),
+        _ => cx.render(rsx!(
+        div {
+            class: "grid grid-cols-1 place-items-center h-[300px]",
+            div {
+                class: "radial-progress animate-spin w-20 h-20",
+                style: "--value:70;",
+                    "..."
+            }
+        })),
     }
 }
 
