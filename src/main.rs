3085d905a062be8d9c359f3413aa9a74eb0893a8
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-03-31 09:35:41 +0900

    another snapshot still buggy

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use {
-    chrono::prelude::*, clap::Parser, dioxus::prelude::*, dioxus_desktop::Config, hyper::Client,
-    hyper_tls::HttpsConnector, serde_json::Value,
+    chrono::prelude::*, clap::Parser, dioxus::prelude::*, dioxus_desktop::Config, serde_json::Value,
 };
 
 #[derive(Clone, Debug, Default, Parser)]
@@ -9,9 +8,9 @@ struct AppConfig {
     /// area code
     #[clap(short = 'a')]
     area: Option<String>,
-    /// service (channel)
-    #[clap(short = 's')]
-    service: Option<String>,
+    // /// service (channel)
+    // #[clap(short = 's')]
+    // service: Option<String>,
     /// date
     #[clap(short = 'd')]
     date: Option<String>,
@@ -21,6 +20,12 @@ struct AppConfig {
     /// Just download the csv w/o GUI
     #[clap(long = "headless")]
     headless: bool,
+}
+
+#[derive(Clone, Debug, Default, Eq, PartialEq, Props)]
+struct AppProps {
+    area: String,
+    apikey: String,
 }
 
 #[tokio::main]
@@ -39,20 +44,38 @@ async fn main() {
     );
 }
 
-#[allow(unused_variables)]
 fn app(cx: Scope) -> Element {
     let app_config = AppConfig::parse();
-    let mut count = use_state(cx, || 0);
-    let channel = use_state(cx, || match &app_config.service {
-        Some(s) => s.clone(),
-        _ => "g1".to_string(),
-    });
-    let json = {
-        let c = app_config.clone();
-        use_future(cx, (count, channel), |(count, channel)| async move {
-            load_json_reqwest(c, channel.get().clone()).await
-        })
-    };
+    cx.render(rsx! {
+        AppBody {
+            area: app_config.area.unwrap_or("400".to_string()),
+            apikey: app_config.apikey,
+        }
+    })
+}
+
+// #[allow(unused_variables)]
+#[allow(non_snake_case)]
+fn AppBody(cx: Scope<AppProps>) -> Element {
+    let json: &UseState<Value> = use_state(cx, || serde_json::from_str("{\"NO\": {}}").unwrap());
+    macro_rules! Fetch {
+        ($service: expr) => {{
+            let c = cx.props.clone();
+            move |_| {
+                cx.spawn({
+                    let c = c.clone();
+                    let ch = $service.to_string();
+                    let j = json.to_owned();
+                    async move {
+                        let resp = load_json_reqwest(c, ch).await;
+                        if let Ok(json) = resp {
+                            j.set(json);
+                        }
+                    }
+                })
+            }
+        }};
+    }
     macro_rules! TAB_CLASS {
         ($target: expr, $service: expr) => {
             if $target == $service {
@@ -62,192 +85,155 @@ fn app(cx: Scope) -> Element {
             }
         };
     }
-    match parse_json(json.value()) {
-        Some((json, ch)) => {
-            cx.render(rsx!{
-                div {
-                    class: "tabs mt-2 ml-2",
-                    button {
-                        class: TAB_CLASS!("g1", ""),
-                        onclick: move |_| { count += 1; channel.set("g1".to_string()) },
-                        "NHK総合1"
-                    },
-                    button {
-                        class: TAB_CLASS!("e1", ""),
-                        onclick: move |_| { count += 1; channel.set("e1".to_string()) },
-                        "NHKEテレ1"
-                    },
-                    button {
-                        class: TAB_CLASS!("r1", ""),
-                        onclick: move |_| { count += 1; channel.set("r1".to_string()) },
-                        "NHKラジオ第1"
-                    },
-                    button {
-                        class: TAB_CLASS!("r2", ""),
-                        onclick: move |_| { count += 1; channel.set("r2".to_string()) },
-                        "NHKラジオ第2"
-                    },
-                    button {
-                        class: TAB_CLASS!("r3", ""),
-                        onclick: move |_| { count += 1; channel.set("r3".to_string()) },
-                        "NHKFM"
-                    },
-                }
-                div {
-                    class: "grid bg-base-300 p-0 mx-2 drop-shadow-xl",
-                    table {
-                        class:"table table-compact p-0 mt-0 w-full text-white bg-red-600 border-red-600 border-y-2 border-solid border-0 border-indigo-600",
-                        tr {
-                            class:"",
-                            th {
-                                "開始時間"
+    cx.render(rsx!{
+        div {
+            class: "tabs mt-2 ml-2",
+            button {
+                class: TAB_CLASS!("g1", ""),
+                onclick: Fetch!("g1"),
+                "NHK総合1"
+            },
+            button {
+                class: TAB_CLASS!("e1", ""),
+                onclick: Fetch!("e1"),
+                "NHKEテレ1"
+            },
+            button {
+                class: TAB_CLASS!("r1", ""),
+                onclick: Fetch!("r1"),
+                "NHKラジオ第1"
+            },
+            button {
+                class: TAB_CLASS!("r2", ""),
+                onclick: Fetch!("r2"),
+                "NHKラジオ第2"
+            },
+            button {
+                class: TAB_CLASS!("r3", ""),
+                onclick: Fetch!("r3"),
+                "NHKFM"
+            },
+        },
+        match parse_json(json.get()) {
+            Some((data, ch)) if !ch.is_empty() => {
+                let refetch = {
+                    let c = cx.props.clone();
+                    move |_| {
+                        cx.spawn({
+                            let c = c.clone();
+                            let ch = ch.to_string();
+                            let js = json.to_owned();
+                            async move {
+                                if let Ok(resp) = load_json_reqwest(c, ch).await {
+                                    js.set(resp);
+                                }
                             }
-                            th {
-                                class:"text-right",
-                                button {
-                                    class: "btn btn-outline btn-sm text-neutral-200",
-                                    onclick: move |_| { count += 1 },
-                                    "表示情報更新"
+                        })
+                    }
+                };
+                rsx!(
+                    div {
+                        class: "grid bg-base-300 p-0 mx-2 drop-shadow-xl",
+                        table {
+                            class:"table table-compact p-0 mt-0 w-full text-white bg-red-600 border-red-600 border-y-2 border-solid border-0 border-indigo-600",
+                            tr {
+                                th { "開始時間" }
+                                th {
+                                    class: "text-right",
+                                    button {
+                                        class: "btn btn-outline btn-sm text-neutral-200",
+                                        onclick: refetch,
+                                        "表示情報更新"
+                                    }
+                                }
+                            }
+                            tr {
+                                class:"bg-slate-100 text-gray-600",
+                                td {
+                                    DateTime::parse_from_rfc3339(data["following"]["start_time"].as_str().unwrap()).unwrap().format("%H:%M").to_string(),
+                                }
+                                td {
+                                    data["following"]["title"].as_str(),
+                                }
+                            }
+                            tr {
+                                class:"bg-slate-100 text-gray-600",
+                                td {
+                                    colspan: 2,
+                                    class: "whitespace-normal pl-8 w-4/5 text-sm",
+                                    data["following"]["subtitle"].as_str(),
+                                }
+                            }
+                            tr {
+                                class:"bg-slate-200 text-black",
+                                td {
+                                    DateTime::parse_from_rfc3339(data["present"]["start_time"].as_str().unwrap()).unwrap().format("%H:%M").to_string(),
+                                }
+                                td {
+                                    data["present"]["title"].as_str(),
+                                }
+                            }
+                            tr {
+                                class: "bg-slate-200 text-black",
+                                td {
+                                    colspan: 2,
+                                    class:"whitespace-normal pl-8 w-4/5 text-sm",
+                                    data["present"]["subtitle"].as_str(),
+                                }
+                            }
+                            tr {
+                                class: "bg-slate-400 text-gray-800",
+                                td {
+                                    DateTime::parse_from_rfc3339(data["previous"]["start_time"].as_str().unwrap()).unwrap().format("%H:%M").to_string(),
+                                }
+                                td {
+                                    data["previous"]["title"].as_str(),
+                                }
+                            }
+                            tr {
+                                class: "bg-slate-400 text-gray-800",
+                                td {
+                                    colspan: 2,
+                                    class:"whitespace-normal pl-8 w-4/5 text-sm",
+                                    data["previous"]["subtitle"].as_str(),
                                 }
                             }
                         }
-                        tr {
-                            class:"bg-slate-100 text-gray-600",
-                            td {
-                                class:"",
-                                DateTime::parse_from_rfc3339(json["following"]["start_time"].as_str().unwrap()).unwrap().format("%H:%M").to_string(),
-                            }
-                            td {
-                                class:"",
-                                json["following"]["title"].as_str(),
-                            }
-                        }
-                        tr {
-                            class:"bg-slate-100 text-gray-600",
-                            td {
-                                colspan: 2,
-                                class:"whitespace-normal pl-8 w-4/5 text-sm",
-                                json["following"]["subtitle"].as_str(),
-                            }
-                        }
-                        tr {
-                            class:"bg-slate-200 text-black",
-                            td {
-                                class:"",
-                                DateTime::parse_from_rfc3339(json["present"]["start_time"].as_str().unwrap()).unwrap().format("%H:%M").to_string(),
-                            }
-                            td {
-                                class:"",
-                                json["present"]["title"].as_str(),
-                            }
-                        }
-                        tr {
-                            class:"bg-slate-200 text-black",
-                            td {
-                                colspan: 2,
-                                class:"whitespace-normal pl-8 w-4/5 text-sm",
-                                json["present"]["subtitle"].as_str(),
-                            }
-                        }
-                        tr {
-                            class:"bg-slate-400 text-gray-800",
-                            td {
-                                class:"",
-                                DateTime::parse_from_rfc3339(json["previous"]["start_time"].as_str().unwrap()).unwrap().format("%H:%M").to_string(),
-                            }
-                            td {
-                                class:"",
-                                json["previous"]["title"].as_str(),
-                            }
-                        }
-                        tr {
-                            class:"bg-slate-400 text-gray-800",
-                            td {
-                                colspan: 2,
-                                class:"whitespace-normal pl-8 w-4/5 text-sm",
-                                json["previous"]["subtitle"].as_str(),
-                            }
-                        }
                     }
+                )
                 }
-            })
-        }
-        _ => {
-            dbg!();
-            cx.render(rsx! {
-                div {
-                    class: "tabs mt-2 ml-2",
-                    button {
-                        class: TAB_CLASS!("g1", ""),
-                        onclick: move |_| { count += 1; channel.set("g1".to_string()) },
-                        "NHK総合1"
-                    },
-                    button {
-                        class: TAB_CLASS!("e1", ""),
-                        onclick: move |_| { count += 1; channel.set("e1".to_string()) },
-                        "NHKEテレ1"
-                    },
-                    button {
-                        class: TAB_CLASS!("r1", ""),
-                        onclick: move |_| { count += 1; channel.set("r1".to_string()) },
-                        "NHKラジオ第1"
-                    },
-                    button {
-                        class: TAB_CLASS!("r2", ""),
-                        onclick: move |_| { count += 1; channel.set("r2".to_string()) },
-                        "NHKラジオ第2"
-                    },
-                    button {
-                        class: TAB_CLASS!("r3", ""),
-                        onclick: move |_| { count += 1; channel.set("r3".to_string()) },
-                        "NHKFM"
-                    },
-                }
-                div {
-                    class: "grid bg-base-300 p-0 mx-2 drop-shadow-xl",
+            Some(_) =>
+                rsx!(
                     div {
-                        class: "grid grid-cols-1 place-items-center h-[300px]",
+                        class: "grid bg-base-300 p-0 mx-2 drop-shadow-xl",
                         div {
-                            class: "radial-progress animate-spin w-20 h-20",
-                            style: "--value:70;",
-                            ""
+                            class: "grid grid-cols-1 place-items-center h-[300px]",
+                            "サービスを選んで下さい"
                         }
                     }
-                }
-            })
+                ),
+            _ =>
+                rsx!(
+                    div {
+                        class: "grid bg-base-300 p-0 mx-2 drop-shadow-xl",
+                        div {
+                            class: "grid grid-cols-1 place-items-center h-[300px]",
+                            div {
+                                class: "radial-progress animate-spin w-20 h-20",
+                                style: "--value:70;",
+                                ""
+                            }
+                        }
+                    }
+                ),
         }
-    }
+    })
 }
 
-#[allow(dead_code)]
-async fn load_json(config: &AppConfig, service: &str) -> hyper::Result<Value> {
-    let client = Client::builder()
-        .retry_canceled_requests(true)
-        .build::<_, hyper::Body>(HttpsConnector::new());
+async fn load_json_reqwest(config: AppProps, service: String) -> hyper::Result<Value> {
     let base = {
         // "https://api.nhk.or.jp/v2/pg/list/{area}/{service}/{date}.json?key={key}"
-        let area = config.area.as_deref().unwrap_or("400");
-        let key = &config.apikey;
-        format!("https://api.nhk.or.jp/v2/pg/now/{area}/{service}.json?key={key}")
-            .parse()
-            .expect("wrong url")
-    };
-    // dbg!();
-    let res = client.get(base).await?;
-    // dbg!(&res);
-    let buf = hyper::body::to_bytes(res).await?;
-    let str = String::from_utf8_lossy(buf.as_ref());
-    assert!(!str.is_empty());
-    // dbg!(&str);
-    let json: Value = serde_json::from_str(str.to_string().as_str()).expect("invalid json");
-    // dbg!(&json);
-    Ok(json)
-}
-
-async fn load_json_reqwest(config: AppConfig, service: String) -> hyper::Result<Value> {
-    let base = {
-        // "https://api.nhk.or.jp/v2/pg/list/{area}/{service}/{date}.json?key={key}"
-        let area = config.area.as_deref().unwrap_or("400");
+        let area = config.area;
         let key = &config.apikey;
         format!("https://api.nhk.or.jp/v2/pg/now/{area}/{service}.json?key={key}")
     };
@@ -260,14 +246,16 @@ async fn load_json_reqwest(config: AppConfig, service: String) -> hyper::Result<
     Ok(json)
 }
 
-fn parse_json(json: Option<&hyper::Result<Value>>) -> Option<(Value, String)> {
-    let Some(Ok(json)) = json else {
-        return None;
-    };
-    for ch in ["g1", "e1", "r1", "r2", "r3"] {
-        if let Some(j) = json["nowonair_list"].get(ch) {
-            return Some((j.clone(), ch.to_string()));
+fn parse_json(json: &Value) -> Option<(Value, String)> {
+    if let Some(list) = json.get("nowonair_list") {
+        for ch in ["g1", "e1", "r1", "r2", "r3"] {
+            if let Some(target) = list.get(ch) {
+                return Some((target.clone(), ch.to_string()));
+            }
         }
+    }
+    if json.get("NO").is_some() {
+        return Some((json.clone(), "".to_string()));
     }
     None
 }
